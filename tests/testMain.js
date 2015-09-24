ad0bdd868c476bae73d481452ca832551755ff75
djstroky <djstroky@yahoo.com>
2015-09-23 19:45:45 -0700

    deferred improvements

--- tests/testMain.js
+++ tests/testMain.js
@@ -9,15 +9,25 @@ module.exports = function(testModel, streamerConfig, callback) {
     var inserter = dbStreamer.getInserter(streamerConfig);
 
     // establish connection
-    inserter.connect(function() {
+    inserter.connect(function(err, client) {
 
       // push some rows
       inserter.push({a: 1, b: 'one', c: new Date() });
       inserter.push({a: 2, b: 'two', c: new Date() });
       inserter.push({a: 3, b: 'three', c: new Date() });
 
+      // create defered inserter
+      streamerConfig.client = client;
+      streamerConfig.deferUntilEnd = true;
+      var deferedInserter = dbStreamer.getInserter(streamerConfig);
+
+      deferedInserter.push({a: 4, b: 'four', c: new Date() });
+      deferedInserter.setEndHandler(callback);
+
       // set end callback
-      inserter.setEndHandler(callback);
+      inserter.setEndHandler(function() {
+        deferedInserter.end();
+      });
 
       // announce end
       inserter.end();
--- tests/testSequelizeBulkInsert.js
+++ tests/testSequelizeBulkInsert.js
@@ -6,24 +6,13 @@ var testMain = require('./testMain'),
 var sequelizeConfig = 'postgres://streamer:streamer@localhost:5432/streamer-test',
   testModel = getTestModel(sequelizeConfig),
   streamerConfig = {
-    dbConnString: sequelizeConfig,
-    tableName: 'test_table',
-    columns: ['a', 'b', 'c'],
     useSequelizeBulkInsert: true,
     sequelizeModel: testModel
   };
 
 describe('sequelize-bulk-insert-load', function() {
-  
   it('bulkInsert should load', function() {
     var testMainPromise = Promise.promisify(testMain);
     return testMainPromise(testModel, streamerConfig);
   });
-
-  it('deferred bulkInsert should load', function() {
-    var testMainPromise = Promise.promisify(testMain);
-    streamerConfig.deferUntilEnd = true;
-    return testMainPromise(testModel, streamerConfig);
-  });
-
 });
