9df14d930c5e4b2cff057cabe6bbda8203215ddf
evansiroky <evan.siroky@yahoo.com>
2016-02-29 12:16:39 -0800

    instructing travis ci to run code climate script

--- scripts/testDelegator.js
+++
@@ -1,7 +0,0 @@
-var exec = require('child_process').exec
-
-if(process.env.DIALECT) {
-  exec('npm run test-code', { env: process.env }, function() {});
-} else if(process.env.COVERAGE) {
-  exec('npm run codeclimate', function() {});
-}
